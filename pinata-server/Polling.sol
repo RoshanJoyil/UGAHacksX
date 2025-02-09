6ffc7bb76383ca12dd243d87cb16406bc1d453d8
1134703 <colinchuazy@icloud.com>
2025-02-08 19:08:28 -0500

    testing more stuff with contract

--- pinata-server/Polling.sol
+++ pinata-server/Polling.sol
@@ -2,10 +2,10 @@ pragma solidity ^0.8.0;
 
 contract Polling {
     struct Poll {
-        string question;
-        string[] options;
-        mapping(uint => uint) votes;
-        mapping(address => bool) hasVoted;
+        string ipfsHash; // IPFS hash of the poll metadata (question and options)
+        string[] options; // Array of poll options
+        mapping(uint => uint) votes; // Tracks vote counts for each option
+        mapping(address => bool) hasVoted; // Tracks whether an address has voted
         bool exists;
         bool closed;
         bool createdByAdmin;
@@ -15,7 +15,7 @@ contract Polling {
     uint public pollCount;
     address public admin;
 
-    event PollCreated(uint pollId, string question, string[] options);
+    event PollCreated(uint pollId, string ipfsHash, bool createdByAdmin);
     event VoteCast(uint pollId, uint optionIndex, address voter);
     event PollClosed(uint pollId);
 
@@ -28,39 +28,41 @@ contract Polling {
         admin = msg.sender; // Set the deployer as admin
     }
 
-    function createPoll(string memory _question, string[] memory _options) public onlyAdmin {
-        require(_options.length > 1, "Poll must have at least two options");
+    // Create a poll with an IPFS hash (Admin-only)
+    function createAdminPoll(string memory _ipfsHash) public onlyAdmin {
+        require(bytes(_ipfsHash).length > 0, "IPFS hash is required");
         pollCount++;
         Poll storage newPoll = polls[pollCount];
-        newPoll.question = _question;
-        newPoll.options = _options;
+        newPoll.ipfsHash = _ipfsHash;
         newPoll.exists = true;
         newPoll.createdByAdmin = true; // Mark as admin-created
-        emit PollCreated(pollCount, _question, _options);
+        emit PollCreated(pollCount, _ipfsHash, true);
     }
 
-    function createUserPoll(string memory _question, string[] memory _options) public {
-        require(_options.length > 1, "Poll must have at least two options");
+    // Create a poll with an IPFS hash (User-level)
+    function createPoll(string memory _ipfsHash) public {
+        require(bytes(_ipfsHash).length > 0, "IPFS hash is required");
         pollCount++;
         Poll storage newPoll = polls[pollCount];
-        newPoll.question = _question;
-        newPoll.options = _options;
+        newPoll.ipfsHash = _ipfsHash;
         newPoll.exists = true;
         newPoll.createdByAdmin = false; // Mark as user-created
-        emit PollCreated(pollCount, _question, _options);
+        emit PollCreated(pollCount, _ipfsHash, false);
     }
 
+    // Vote on a poll
     function vote(uint _pollId, uint _optionIndex) public {
         require(polls[_pollId].exists, "Poll does not exist");
         require(!polls[_pollId].closed, "Poll is closed");
         require(!polls[_pollId].hasVoted[msg.sender], "Already voted");
-        require(_optionIndex < polls[_pollId].options.length, "Invalid option");
-        
+
         polls[_pollId].votes[_optionIndex]++;
         polls[_pollId].hasVoted[msg.sender] = true;
+
         emit VoteCast(_pollId, _optionIndex, msg.sender);
     }
 
+    // Close a poll
     function closePoll(uint _pollId) public onlyAdmin {
         require(polls[_pollId].exists, "Poll does not exist");
         require(!polls[_pollId].closed, "Poll is already closed");
@@ -69,37 +71,25 @@ contract Polling {
         emit PollClosed(_pollId);
     }
 
+    // Get the results of a poll
     function getResults(uint _pollId) public view returns (uint[] memory) {
         require(polls[_pollId].exists, "Poll does not exist");
 
-        uint[] memory results = new uint[](polls[_pollId].options.length);
-        for (uint i = 0; i < polls[_pollId].options.length; i++) {
+        uint[] memory results = new uint[](16);
+        for (uint i = 0; i < 16; i++) {
             results[i] = polls[_pollId].votes[i];
         }
         return results;
     }
-    function getAdminPolls() public view returns (uint[] memory) {
-    uint[] memory adminPolls = new uint[](pollCount);
-    uint count = 0;
-    for (uint i = 1; i <= pollCount; i++) {
-        if (polls[i].createdByAdmin) {
-            adminPolls[count] = i;
-            count++;
-        }
-    }
-    return adminPolls;
-}
 
-    function getUserPolls() public view returns (uint[] memory) {
-        uint[] memory userPolls = new uint[](pollCount);
-        uint count = 0;
-        for (uint i = 1; i <= pollCount; i++) {
-            if (!polls[i].createdByAdmin) {
-                userPolls[count] = i;
-                count++;
-            }
-        }
-        return userPolls;
+    // Get the IPFS hash of a poll
+    function getPollIPFSHash(uint _pollId) public view returns (string memory) {
+        require(polls[_pollId].exists, "Poll does not exist");
+        return polls[_pollId].ipfsHash;
     }
 
+    function getOptions(uint _pollId) public view returns (string[] memory) {
+        require(polls[_pollId].exists, "Poll does not exist");
+        return polls[_pollId].options;
+    }
 }
